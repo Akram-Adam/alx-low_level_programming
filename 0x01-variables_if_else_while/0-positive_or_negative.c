c7b67fade84e8a89a1259d696282b4f9b87fd3db
Akram-Adam <akramadam050@gmail.com>
2023-09-11 04:33:44 -0700

    My first commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,12 +5,11 @@
 #include <stdio.h>
 
 /**
- * *main - entry point
+ * * main - entry point
  *
  * * main - Determines if a number is positive, negative or zero.
  *
  * * Return: Always 0 (Success)
- *
 */
 
 int main(void)
