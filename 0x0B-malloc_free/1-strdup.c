6571fd298174d705cb3dd0e56b725db23bd58532
Akram-Adam <akramadam050@gmail.com>
2023-10-04 15:05:29 -0700

    My first commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+/**
+ * _strdup - copy
+ * @str: pointer to string
+ * Return: pointer
+ */
+
+char *_strdup(char *str)
+{
+char *s;
+int i = 0, j;
---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * argstostr - prints args
+ * @ac: takes in width of grid
+ * @av: height of grid
+ * Return: the args one line at a time
+ */
+
+char *argstostr(int ac, char **av)
+{
+char *str;
+int count = 0, a = 0, b = 0, c = 0;
