9c6732572c1a13e4d01b14cf6ed99a6ca46c8220
Akram-Adam <akramadam050@gmail.com>
2023-10-04 16:54:29 -0700

    My first commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,12 +12,13 @@
 
 char *create_array(unsigned int size, char c)
 {
+	char *array;
 	unsigned int i;
 
 	if (size == 0)
 		return (NULL);
 
-	char array = malloc(sizeof(char) * size);
+	array = (char *)malloc(sizeof(char) * size);
 
 	if (array == NULL)
 		return (NULL);
