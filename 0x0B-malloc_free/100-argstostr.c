dc4d8ce2c20d3ec5e99db83ba83f81aeb52405ad
Akram-Adam <akramadam050@gmail.com>
2023-10-04 17:31:34 -0700

    My first commit

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,12 +1,46 @@
 #include "main.h"
-/**
- * argstostr - prints args
- * @ac: takes in width of grid
- * @av: height of grid
- * Return: the args one line at a time
- */
+#include <stdlib.h>
 
+/**
+ * argstostr - concatenates all the arguments of your program.
+ * @ac: arguments count
+ * @av: arguments vector
+ *
+ * Return: a pointer to a new string, or NULL if it fails
+ */
 char *argstostr(int ac, char **av)
 {
-char *str;
-int count = 0, a = 0, b = 0, c = 0;
+char *str, *s;
+int i, j, k, len = 0;
+
+if (ac == 0 || av == NULL)
+return (NULL);
+for (i = 0; i < ac; i++)
+{
+s = av[i];
+j = 0;
+
+while (s[j++])
+len++;
+len++;
+}
+
+str = (char *)malloc(sizeof(char) * (len + 1));
+if (str == NULL)
+return (NULL);
+
+for (i = 0, j = 0; i < ac && j < len; i++)
+{
+s = av[i];
+k = 0;
+while (s[k])
+{
+str[j] = s[k];
+k++;
+j++;
+}
+str[j++] = '\n';
+}
+str[j] = '\0';
+return (str);
+}
--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,12 +1,38 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
+
 /**
- * alloc_grid - prints a grid of integers
- * @width: width of the grid
- * @height: height of the grid
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers.
+ * @width: width of array
+ * @height: height of array
  *
- * Return: pointer..
+ * Return: a pointer to a 2 dimensional array of integers.
  */
 int **alloc_grid(int width, int height)
 {
-int **s, r, c;
+int **array, i, j;
+int len = width * height;
+
+if (len <= 0)
+return (NULL);
+
+array = (int **)malloc(sizeof(int *) * height);
+if (array == NULL)
+0return (NULL);
+
+for (i = 0; i < height; i++)
+array[i] = (int *)malloc(sizeof(int) * width);
+if (array[i] == NULL;
+{
+for (i--; i >= 0; i--)
+free(array[i]);
+free(array);
+return (NULL);
+}
+}
+
+for (i = 0; i < height; i++)
+for (j = 0; j < width; j++)
+array[i][j] = 0;
+return (array);
+}
