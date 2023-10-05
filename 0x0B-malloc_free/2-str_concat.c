cc3e28d23030396a64e2f6443e22456128dc9968
Akram-Adam <akramadam050@gmail.com>
2023-10-05 15:19:30 -0700

    My first commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,12 +1,46 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * _strlen - length of a string
- * @s: input char
- * Return: length of a string
+ * str_concat - concatenates two strings.
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: a pointer to a newly allocated space in memory which
+ * contains the contents of s1, followed by the contents of s2,
+ * and null terminated. NULL on failure
  */
-
-int _strlen(char *s)
+char *str_concat(char *s1, char *s2)
 {
-int l = 0;
+int i, j, len1, len2, len;
+char *result;
 
+len1 = len2 = 0;
+
+if (s1 != NULL)
+{
+i = 0;
+while (s1[i++] != '\0')
+len1++;
+}
+
+if (s2 != NULL)
+{
+i = 0;
+while (s2[i++] != '\0')
+len2++;
+}
+
+len = len1 + len2;
+result = (char *)malloc(sizeof(char) * (len + 1));
+if (result == NULL)
+return (NULL);
+
+for (i = 0; i < len1; i++)
+result[i] = s1[i];
+for (j = 0; j < len2; j++, i++)
+result[i] = s2[j];
+result[len] = '\0';
+return (result);
+}
--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,12 +1,16 @@
+#include "main.h"
 #include <stdlib.h>
 
 /**
- * free_grid - alloc_grid
- * @grid: pointer to 2D arrays
- * @height: array
- * Return: void
+ * free_grid - frees a 2 dimensional grid
+ * @grid: the grid
+ * @height: height of the grid
  */
-
 void free_grid(int **grid, int height)
 {
-height--;
+int i;
+
+for (i = 0; i < height; i++)
+free(grid[i]);
+free(grid);
+}
