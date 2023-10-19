4d8aaf2066b5af495e2c10e9754ec55308811ee5
Akram-Adam <akramadam050@gmail.com>
2023-10-19 05:21:05 -0700

    My first commit

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,22 +1,21 @@
 #include "function_pointers.h"
-
 /**
- * array_iterator - a function given as a
- * parameter on each element of an array.
- *
- * @array: array to execute func on
- * @size: is the size of the array
- * @action:  is a pointer to the function you need to use
- *
- * Return: void
- */
+ * array_iterator - this functions iterates an array
+(* a blank line
+ *@array: the array to iterate
+ *@size: the size of the array.
+ *@action: this a function pointer.
+* Description: this function iterate an array)?
+(* section header: the header of this function is function_pointers.h)*
+* Return: this is a void function no return
+*/
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-size_t i;
+	size_t i;
 
-if (array && size && action)
-{
-for (i = 0; i < size; i++)
-action(*(array + i));
-}
+	if (array && size && action)
+	{
+	for (i = 0; i < size; i++)
+		action(*(array + i));
+	}
 }
--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,49 +1,37 @@
-#include <stdlib.h>
-#include <stdio.h>
-
+#include<stdio.h>
+#include<stdlib.h>
 /**
- * print_opcodes - print the opcodes of this program
- * @a: address of the main function
- * @n: number of bytes to print
- *
- * Return: void
- */
-void print_opcodes(char *a, int n)
+ * main- Entry point
+(* a blank line
+ *@argc: the number of parameters.
+ *@argv: the parameeters in the case the number ob bytes.
+* Description: this program prints opcodes in hexa)?
+* Return: 0 in succes
+*/
+int main(int argc, char *argv[])
 {
-int i;
+	int i, n;
 
-for (i = 0; i < n; i++)
-{
-printf("%.2hhx", a[i]);
-if (i < n - 1)
-printf(" ");
-}
-printf("\n");
 
-}
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	n = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
 
-/**
- * main - prints the opcodes of its own main function
- * @argc: number of arguments passed to the function
- * @argv: array of pointers to arguments
- *
- * Return: always O
- */
-int main(int argc, char **argv)
-{
-int n;
-
-if (argc != 2)
-{
-printf("Error\n");
-exit(1);
-}
-n = atoi(argv[1]);
-if (n < 0)
-{
-printf("Error\n");
-exit(2);
-}
-print_opcodes((char *)&main, n);
-return (0);
+	for (i = 0; i < n; i++)
+	{
+		printf("%02hhx", *((char *)main + i));
+		if (i < n - 1)
+			printf(" ");
+		else
+			printf("\n");
+	}
+	return (0);
 }
